990f56b2075cd7ef17d342335420245369a1d037
Sebastian Arango <sjarango92@gmail.com>
2019-09-03 11:48:39 -0400

    added functions and crystal divs

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+
+var gameScore;
+var userScore = 0;
+var wins = 0;
+var losses = 0;
+var crystals = [null, null, null, null];
+
+// Math.floor(Math.random() * ((y-x)+1) + x);
+// x = start , y = end
+// random number between 9 - 120
+function scoreNumber() {
+    var randomNum = Math.floor(Math.random() * ((120-9) + 1) + 9); 
+    return randomNum;
+}
+// random number between 1 - 12
+function crystalNumber() {
+    var randomNum = ( Math.floor(Math.random() * 12) ) + 1; 
+    return randomNum;
+}
+
+function setCrystals(){
+    for (var i=0; i<crystals.length; i++) {
+        crystals[i] = crystalNumber();
+        console.log("crystal[" + i + "] :" + crystals[i] );
+    }
+}
+
+function reset() {
+    gameScore = scoreNumber();
+    userScore = 0;
+    setCrystals();
+}
+
+setCrystals();
